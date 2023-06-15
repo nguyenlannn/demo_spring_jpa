c7bf86373188fb55b43cb83f07803bf8d696faad
nguen thi lan <lannt5@rikkeisoft.com>
2023-06-15 15:55:35 +0700

    tao tai khoan2

--- src/main/java/com/example/lan_demo/controller/UserController.java
+++ src/main/java/com/example/lan_demo/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.example.lan_demo.controller;
 
-import com.example.lan_demo.dto.req.LoginReq;
 import com.example.lan_demo.dto.req.UserReq;
 import com.example.lan_demo.dto.res.UserRes;
 import com.example.lan_demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/lan_demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/lan_demo/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.example.lan_demo.exception.BadRequestException;
 import com.example.lan_demo.repository.UserRepository;
 import com.example.lan_demo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
